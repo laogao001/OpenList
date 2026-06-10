6c4568e5663f8fccec17004214d29b166b21afec
高辉 <515256522@qq.com>
2026-06-10 11:59:34 +0800

    feat(embyToLocalPlayer): set auto update url to github raw link and bump version

--- embyToLocalPlayer.user.js
+++ embyToLocalPlayer.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   embyToLocalPlayer
 // @name:en      embyToLocalPlayer
 // @namespace    https://github.com/kjtsune/embyToLocalPlayer
-// @version      2026.02.12
+// @version      2026.02.13
 // @description  Emby/Jellyfin 调用外部本地播放器，并回传播放记录。适配 Plex。
 // @description:zh-CN Emby/Jellyfin 调用外部本地播放器，并回传播放记录。适配 Plex。
 // @description:en  Play in an external player. Update watch history to Emby/Jellyfin server. Support Plex.
@@ -26,6 +26,8 @@
 // @run-at       document-start
 // @connect      127.0.0.1
 // @license MIT
+// @downloadURL  https://raw.githubusercontent.com/laogao001/OpenList/main/embyToLocalPlayer.user.js
+// @updateURL    https://raw.githubusercontent.com/laogao001/OpenList/main/embyToLocalPlayer.user.js
 // ==/UserScript==
 'use strict';
 /*global ApiClient*/
