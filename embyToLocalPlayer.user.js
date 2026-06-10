563a1806390096650c157d110b292fac48a49275
高辉 <515256522@qq.com>
2026-06-10 12:00:29 +0800

    chore(embyToLocalPlayer): update version to current date 2026.06.10

--- embyToLocalPlayer.user.js
+++ embyToLocalPlayer.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   embyToLocalPlayer
 // @name:en      embyToLocalPlayer
 // @namespace    https://github.com/kjtsune/embyToLocalPlayer
-// @version      2026.02.13
+// @version      2026.06.10
 // @description  Emby/Jellyfin 调用外部本地播放器，并回传播放记录。适配 Plex。
 // @description:zh-CN Emby/Jellyfin 调用外部本地播放器，并回传播放记录。适配 Plex。
 // @description:en  Play in an external player. Update watch history to Emby/Jellyfin server. Support Plex.
